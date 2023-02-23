9fcc1f35483fe735bfb023880d34be71da7bd319
Ben Hsieh <bqh5026@gmail.com>
2023-02-23 17:22:09 -0600

    Add flexbox

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <Button title='Tap Me' />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
+        <Button title='Add Goad' />
+      </View>
+      <View>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+    // borderWidth: 2,
+    // borderColor: 'red', 
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+  }
 });
